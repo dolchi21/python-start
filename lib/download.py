ed0933ad1482fe786777c19e9710e1aef031f3cd
Axel Dolce <axel.dolce@gmail.com>
2018-10-11 19:05:13 -0300

    update

--- lib/download.py
+++ lib/download.py
@@ -1,9 +1,14 @@
-from requests import get
+import requests
 
 
-def download(source: str, dest: int):
-    res = get(source)
+def download(source: str):
+    res = requests.get(source)
+    return res.content
+
+
+def downloadToFile(source: str, dest: int):
+    content = download(source)
     file = open(dest, 'wb')
-    file.write(res.content)
+    file.write(content)
     file.close()
     return file
